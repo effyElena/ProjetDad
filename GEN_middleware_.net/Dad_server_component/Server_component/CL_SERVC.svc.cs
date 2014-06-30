5477e00a12bbe4d45b302ab1cef1127dd4ce7716
billiere david <david.billiere@viacesi.fr>
2014-06-30 11:40:50 +0200

    convention de nommage

---
+++ GEN_middleware_.net/Dad_server_component/Server_component/CL_SERVC.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server_component.Server_component
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "SERVC" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez SERVC.svc ou SERVC.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class CL_SERVC : I_SERVC
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ GEN_middleware_.net/Dad_server_component/Server_component/CL_SERVM_Auth.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server_component.Server_component
+{
+    public class CL_SERVM_Auth
+    {
+    }
+}
